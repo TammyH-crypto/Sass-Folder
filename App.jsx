e71b132539584a06ee509a90e382d47a4e9c653b
Tammy Higgins <tmhiggins1986@gmail.com>
2025-05-13 18:14:39 -0500

    added webpack and babel

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Header from "./src/components/Header.jsx";
+import Main from "./src/components/Main.jsx";
+import Footer from "./src/components/Footer.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
